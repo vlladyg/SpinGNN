425236a69ef1842cd5d0da9fe370d01dfb0250e6
dreamnewworld <vladygin@caltech.edu>
2024-11-29 20:27:54 -0800

    Fixed reordering

--- allegro/allegro/_spin_embedding.py
+++ allegro/allegro/_spin_embedding.py
@@ -115,8 +115,8 @@ class SphericalHarmonicEdgeAttrsTENN(GraphModuleMixin, torch.nn.Module):
         edge_sh_normalization: str = "component",
         edge_sh_normalize: bool = True,
         irreps_in=None,
-        out_field: str = 'edge_attr_tmp',
-        #out_field: str = AtomicDataDict.EDGE_ATTRS_KEY,
+        #out_field: str = 'edge_attr_tmp',
+        out_field: str = AtomicDataDict.EDGE_ATTRS_KEY,
     ):
         super().__init__()
         self.out_field = out_field
